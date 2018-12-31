de378781af9472b64da33f5cd9dc3e9f2abacba1
Michael Schinis <mschinis@gmail.com>
2018-12-31 17:23:17 +0000

    fix(readme): Improve logger color clear

--- src/_utils/logger.js
+++ src/_utils/logger.js
@@ -1,9 +1,9 @@
 function greenLog() {
-    console.log('\x1b[32m', ...arguments);
+    console.log('\x1b[32m%s\x1b[0m', ...arguments);
 }
 
 function redLog() {
-    console.log('\x1b[31m', ...arguments);
+    console.log('\x1b[31m%s\x1b[0m', ...arguments);
 }
 
 module.exports = { greenLog, redLog };
