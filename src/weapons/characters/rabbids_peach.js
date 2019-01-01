034e37972c459bc9d7969b8693ca00395a795e48
Michael Schinis <mschinis@gmail.com>
2019-01-01 02:23:11 +0000

    feat(rabbids_peach): Map rabbids peach weapons on to the save file

--- src/weapons/characters/rabbids_peach.js
+++ src/weapons/characters/rabbids_peach.js
@@ -9,20 +9,21 @@ module.exports = {
         { name: 'Rainbow Runner',        index: 2  },
         { name: 'Loaded Question',       index: 3  },
         { name: 'Destruction Zone',      index: 4  },
-        { name: 'Bling Machine',         index: 5  },
-        { name: 'Brute',                 index: 6  },
-        { name: 'Pain Sprayer',          index: 7  },
-        { name: `Birchin' Urchin`,       index: 8  },
-        { name: 'Royal Pain',            index: 9  },
-        { name: 'Comet',                 index: 10 },
-        { name: 'No Pitty Kitty',        index: 11 },
-        { name: 'Camouflage Corsage',    index: 12 },
-        { name: 'Electron',              index: 13 },
-        { name: 'Ballistic Mystic',      index: 14 },
-        { name: 'Run for the Hills',     index: 15 },
-        { name: 'Pay the Piper',         index: 16 },
-        { name: 'Lucky Devil',           index: 17 },
-        { name: 'New Wave Wrecker',      index: 18 }
+        // 5
+        { name: 'Bling Machine',         index: 6  },
+        { name: 'Brute',                 index: 7  },
+        { name: 'Pain Sprayer',          index: 8  },
+        { name: `Birchin' Urchin`,       index: 9  },
+        { name: 'Royal Pain',            index: 10  },
+        { name: 'Comet',                 index: 11 },
+        { name: 'No Pitty Kitty',        index: 12 },
+        { name: 'Camouflage Corsage',    index: 13 },
+        { name: 'Electron',              index: 14 },
+        { name: 'Ballistic Mystic',      index: 15 },
+        { name: 'Run for the Hills',     index: 16 },
+        { name: 'Pay the Piper',         index: 17 },
+        { name: 'Lucky Devil',           index: 18 },
+        { name: 'New Wave Wrecker',      index: 19 }
     ],
     secondary: [
         { name: 'Excavator',            index: 0  },
@@ -30,14 +31,17 @@ module.exports = {
         { name: 'Sandbagger',           index: 2  },
         { name: 'Low Blow',             index: 3  },
         { name: 'Hot Rod',              index: 4  },
-        { name: 'Declogger Flogger',    index: 5  },
-        { name: 'Buck',                 index: 6  },
-        { name: `Cookies n' Scream`,    index: 7  },
-        { name: 'Anchors Aweigh',       index: 8  },
-        { name: 'Avalanche',            index: 9  },
-        { name: 'Bullseye',             index: 10 },
-        { name: 'Camo Whammo',          index: 11 },
-        { name: 'Alarm Clock',          index: 12 },
-        { name: 'Iron Curtain',         index: 13 }
+        // 5
+        { name: 'Declogger Flogger',    index: 6  },
+        { name: 'Buck',                 index: 7  },
+        { name: `Cookies n' Scream`,    index: 8  },
+        { name: 'Anchors Aweigh',       index: 9  },
+        { name: 'Avalanche',            index: 10 },
+        // 11
+        // 12
+        { name: 'Bullseye',             index: 13 },
+        { name: 'Camo Whammo',          index: 14 },
+        { name: 'Alarm Clock',          index: 15 },
+        { name: 'Iron Curtain',         index: 16 }
     ]
 };
