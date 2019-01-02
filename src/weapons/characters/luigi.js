daaf1db04c476d7a6831f9cf66b9963f7366840d
Michael Schinis <mschinis@gmail.com>
2019-01-02 01:28:22 +0000

    feat(characters): Adds luigi

--- src/weapons/characters/luigi.js
+++ src/weapons/characters/luigi.js
@@ -7,20 +7,23 @@ module.exports = {
         { name: 'Suction Destruction',      index: 0  },
         { name: 'Prime Mover',              index: 1  },
         { name: 'Muffler Snuffler',         index: 2  },
-        { name: 'Spike Strike',             index: 3  },
-        { name: 'Bozo Blaster',             index: 4  },
-        { name: 'Firefighter',              index: 5  },
-        { name: 'Golden Touch',             index: 6  },
-        { name: 'Hog Wild',                 index: 7  },
-        { name: `HMS Let's-a-Go`,           index: 8  },
-        { name: 'Ryu',                      index: 9  },
-        { name: 'Stacker Attacker',         index: 10 },
-        { name: 'Harvey Brawlbanger',       index: 11 },
-        { name: 'BagSniper',                index: 12 },
-        { name: `Lil' Tikes Load n' Go`,    index: 13 },
-        { name: `Luigi's Longshot`,         index: 14 },
-        { name: 'Denim Destroyer',          index: 15 },
-        { name: 'Up Close & Personal',      index: 16 }
+        // Weapon at index 3 hidden from menu
+        { name: 'Spike Strike',             index: 4  },
+        { name: 'Bozo Blaster',             index: 5  },
+        { name: 'Firefighter',              index: 6  },
+        { name: 'Golden Touch',             index: 7  },
+        { name: 'Hog Wild',                 index: 8  },
+        { name: `HMS Let's-a-Go`,           index: 9  },
+        { name: 'Ryu',                      index: 10  },
+        // Weapon at index 11 hidden from menu
+        { name: 'Stacker Attacker',         index: 12 },
+        { name: 'Harvey Brawlbanger',       index: 13 },
+        { name: 'BagSniper',                index: 14 },
+        { name: `Lil' Tikes Load n' Go`,    index: 15 },
+        // Weapon at index 16 hidden from menu
+        { name: `Luigi's Longshot`,         index: 17 },
+        { name: 'Denim Destroyer',          index: 18 },
+        { name: 'Up Close & Personal',      index: 19 }
     ],
     secondary: [
         { name: 'Top Gear',                 index: 0  },
@@ -28,14 +31,18 @@ module.exports = {
         { name: 'Master Control',           index: 2  },
         { name: 'Barn Burner',              index: 3  },
         { name: 'Bwahntasaurus Wrecks',     index: 4  },
-        { name: 'Crater Maker',             index: 5  },
-        { name: 'Danger Arranger',          index: 6  },
-        { name: 'Painville Express',        index: 7  },
-        { name: 'Fatal Frame',              index: 8  },
-        { name: 'Gold Rush',                index: 9  },
-        { name: 'Road Reaper',              index: 10 },
-        { name: 'A Slay at the Beach',      index: 11 },
-        { name: 'Deploy Toy',               index: 12 },
-        { name: 'Attack from the Future II',index: 13 }
+        // Weapon at index 5 hidden from menu
+        // Weapon at index 6 hidden from menu
+        { name: 'Crater Maker',             index: 7  },
+        { name: 'Danger Arranger',          index: 8  },
+        // Weapon at index 9 hidden from menu
+        { name: 'Painville Express',        index: 10  },
+        { name: 'Fatal Frame',              index: 11  },
+        { name: 'Gold Rush',                index: 12  },
+        { name: 'Road Reaper',              index: 13 },
+        { name: 'A Slay at the Beach',      index: 14 },
+        // Weapon at index 15 hidden from menu
+        { name: 'Deploy Toy',               index: 16 },
+        { name: 'Attack from the Future II',index: 17 }
     ]
 };
